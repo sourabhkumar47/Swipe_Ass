bd560dc9c3f377802eb8d003bca2ee24db499afe
Sourabh Kumar <sourabh.47512@gmail.com>
2025-01-30 23:27:38 +0530

    added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id ("com.google.dagger.hilt.android")
+    id ("kotlin-kapt")
 }
 
 android {
@@ -56,4 +58,36 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Core
+    implementation ("androidx.core:core-ktx:1.12.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation ("androidx.core:core-splashscreen:1.0.1")
+
+    // Compose
+    implementation ("androidx.activity:activity-compose:1.8.0")
+    implementation ("androidx.compose.material3:material3:1.3.1")
+    implementation ("androidx.navigation:navigation-compose:2.8.6")
+
+    // Dagger Hilt
+    implementation ("com.google.dagger:hilt-android:2.50")
+    kapt ("com.google.dagger:hilt-compiler:2.48")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    // Room
+    implementation ("androidx.room:room-runtime:2.6.1")
+    kapt ("androidx.room:room-compiler:2.6.0")
+    implementation ("androidx.room:room-ktx:2.6.1")
+
+    // Coil
+    implementation ("io.coil-kt:coil-compose:2.4.0")
+
+    // WorkManager
+    implementation ("androidx.work:work-runtime-ktx:2.10.0")
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+
+    //dagger
+    id("com.google.dagger.hilt.android") version "2.50" apply false
 }
